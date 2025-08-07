ae77ce24dfe9e8e99ed9cb61c033170f43947af2
Wei Lu <luwei.here@gmail.com>
2025-08-07 11:24:28 +0800

    Add activity admin CRUD E2E tests

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -17,3 +17,40 @@ Cypress.Commands.add('login_admin_interface', () => {
     cy.contains('Site administration').should('be.visible')
   })
 })
+
+Cypress.Commands.add('deleteActivities', (activityNames) => {
+  cy.visit('/api/admin/social_protection/activity/');
+  cy.get('body').then(($body) => {
+    let checkedAny = false;
+    activityNames.forEach(activityName => {
+      if ($body.find(`td.field-name:contains("${activityName}")`).length) {
+        // Check the checkbox in the same row as the activity name
+        cy.contains('td.field-name', activityName)
+          .parent('tr')
+          .find('input[type="checkbox"]')
+          .check();
+        checkedAny = true;
+      }
+    });
+
+    if (!checkedAny) {
+      Cypress.log({
+        name: 'deleteActivity',
+        message: `Activities not found, nothing to delete`,
+      });
+      return
+    }
+
+    // Select the delete action and submit
+    cy.get('select[name="action"]').select('delete_selected');
+    cy.get('button[type="submit"]').contains('Go').click();
+
+    // Confirm the deletion
+    cy.get('input[type="submit"][value*="Yes"]').click()
+
+    // Verify deletion
+    activityNames.forEach(activityName => {
+      cy.contains('td.field-name', activityName).should('not.exist');
+    });
+  });
+});
