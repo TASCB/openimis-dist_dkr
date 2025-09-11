bf0b0cc5b33ccc2d44c98d4e6dd7c3325e3eddb7
Wei Lu <luwei.here@gmail.com>
2025-09-11 08:56:14 -0400

    Reorganize cash transfer tests to share setup steps

--- cypress/e2e/cash-trasfer.cy.js
+++ cypress/e2e/cash-trasfer.cy.js
@@ -1,6 +1,6 @@
 import { getProgramTerm } from '../support/utils';
 
-describe('Cash transfer workflows', () => {
+describe('Cash transfer program creation workflows', () => {
   let testProgramNames = [];
 
   beforeEach(function () {
@@ -34,68 +34,6 @@ describe('Cash transfer workflows', () => {
     cy.checkProgramFieldValuesInListView(programCode, programName, maxBeneficiaries, programType)
   })
 
-  it('Updates an individual program', function () {
-    // Disable maker checker
-    cy.loginAdminInterface()
-    cy.setModuleConfig('social_protection', 'social-protection-config.json')
-
-    const programCode = 'E2EICPU'
-    const programName = 'E2E Individual Cash Program Updated'
-    const maxBeneficiaries = "100"
-    const programType = "INDIVIDUAL"
-
-    cy.createProgram(programCode, programName, maxBeneficiaries, programType)
-
-    // Ensure the created program gets cleaned up later
-    testProgramNames.push(programName);
-
-    const updatedProgramCode = 'E2EICP42'
-    const updatedMaxBeneficiaries = "111"
-    const updatedInstitution = "Social Protection Agency"
-    const updatedDescription = "Foo bar baz"
-
-    // Go back into the list page to find the program & edit
-    cy.visit('/front/benefitPlans');
-    cy.contains('tfoot', 'Rows Per Page')
-    cy.contains('td', programName)
-      .parent('tr').within(() => {
-        // click on edit button
-        cy.get('a.MuiIconButton-root').click()
-      })
-
-    cy.assertMuiInput('Code', programCode)
-    cy.enterMuiInput('Code', updatedProgramCode)
-    cy.assertMuiInput('Code', updatedProgramCode)
-
-    cy.enterMuiInput('Max Beneficiaries', updatedMaxBeneficiaries)
-
-    cy.enterMuiInput('Institution', updatedInstitution)
-
-    cy.enterMuiInput('Description', updatedDescription, 'textarea')
-
-    cy.get('[title="Save changes"] button').click()
-
-    // Wait for update to complete
-    cy.get('ul.MuiList-root li div[role="progressbar"]').should('exist')
-    cy.get('ul.MuiList-root li div[role="progressbar"]').should('not.exist')
-
-    // Check last journal message
-    cy.get('ul.MuiList-root li').first().click()
-    cy.contains(`Update ${getProgramTerm()}`).should('exist')
-    cy.contains('Failed to update').should('not.exist')
-
-    // Check program field values are persisted
-    cy.reload()
-    cy.checkProgramFieldValues(
-      updatedProgramCode,
-      programName,
-      updatedMaxBeneficiaries,
-      programType,
-      updatedInstitution,
-      updatedDescription,
-    )
-  })
-
   it('Creates and deletes an household program', function () {
     const programCode = 'E2EGCP'
     const programName = 'E2E Household Cash Program'
@@ -115,87 +53,162 @@ describe('Cash transfer workflows', () => {
     cy.visit('/front/benefitPlans');
     cy.checkProgramFieldValuesInListView(programCode, programName, maxBeneficiaries, programType)
   })
+})
 
-  it('Updates a household program', function () {
+describe('Cash transfer program update workflows', () => {
+  before(() => {
     // Disable maker checker
     cy.loginAdminInterface()
     cy.setModuleConfig('social_protection', 'social-protection-config.json')
+    cy.logoutAdminInterface()
+  })
 
+  describe('Individual program', () => {
+    const programCode = 'E2EICPU'
+    const programName = 'E2E Individual Cash Program Updated'
+    const maxBeneficiaries = "100"
+    const programType = "INDIVIDUAL"
+
+    before(() => {
+      cy.login()
+      cy.createProgram(programCode, programName, maxBeneficiaries, programType)
+    })
+
+    after(() => {
+      cy.deleteProgram(programName)
+    })
+
+    it('Updates an individual program', function () {
+      const updatedProgramCode = 'E2EICP42'
+      const updatedMaxBeneficiaries = "111"
+      const updatedInstitution = "Social Protection Agency"
+      const updatedDescription = "Foo bar baz"
+
+      // Go back into the list page to find the program & edit
+      cy.visit('/front/benefitPlans');
+      cy.contains('tfoot', 'Rows Per Page')
+      cy.contains('td', programName)
+        .parent('tr').within(() => {
+          // click on edit button
+          cy.get('a.MuiIconButton-root').click()
+        })
+
+      cy.assertMuiInput('Code', programCode)
+      cy.enterMuiInput('Code', updatedProgramCode)
+      cy.assertMuiInput('Code', updatedProgramCode)
+
+      cy.enterMuiInput('Max Beneficiaries', updatedMaxBeneficiaries)
+
+      cy.enterMuiInput('Institution', updatedInstitution)
+
+      cy.enterMuiInput('Description', updatedDescription, 'textarea')
+
+      cy.get('[title="Save changes"] button').click()
+
+      // Wait for update to complete
+      cy.get('ul.MuiList-root li div[role="progressbar"]').should('exist')
+      cy.get('ul.MuiList-root li div[role="progressbar"]').should('not.exist')
+
+      // Check last journal message
+      cy.get('ul.MuiList-root li').first().click()
+      cy.contains(`Update ${getProgramTerm()}`).should('exist')
+      cy.contains('Failed to update').should('not.exist')
+
+      // Check program field values are persisted
+      cy.reload()
+      cy.checkProgramFieldValues(
+        updatedProgramCode,
+        programName,
+        updatedMaxBeneficiaries,
+        programType,
+        updatedInstitution,
+        updatedDescription,
+      )
+    })
+  })
+
+  describe('Household program', () => {
     const programCode = 'E2EGCPU'
     const programName = 'E2E Household Cash Program Updated'
     const maxBeneficiaries = "200"
     const programType = "GROUP"
 
-    cy.createProgram(programCode, programName, maxBeneficiaries, programType)
+    before(() => {
+      cy.login()
+      cy.createProgram(programCode, programName, maxBeneficiaries, programType)
+    })
 
-    // Ensure the created program gets cleaned up later
-    testProgramNames.push(programName);
+    after(() => {
+      cy.deleteProgram(programName)
+    })
 
-    const updatedProgramCode = 'E2EGCP42'
-    const updatedMaxBeneficiaries = "222"
-    const updatedInstitution = "Family Support Services"
-    const updatedDescription = "A functional program"
+    it('Updates a household program', function () {
+      const updatedProgramCode = 'E2EGCP42'
+      const updatedMaxBeneficiaries = "222"
+      const updatedInstitution = "Family Support Services"
+      const updatedDescription = "A functional program"
 
-    // Go back into the list page to find the program & edit
-    cy.visit('/front/benefitPlans');
-    cy.contains('tfoot', 'Rows Per Page')
-    cy.contains('td', programName)
-      .parent('tr').within(() => {
-        // click on edit button
-        cy.get('a.MuiIconButton-root').click()
-      })
+      // Go back into the list page to find the program & edit
+      cy.visit('/front/benefitPlans');
+      cy.contains('tfoot', 'Rows Per Page')
+      cy.contains('td', programName)
+        .parent('tr').within(() => {
+          // click on edit button
+          cy.get('a.MuiIconButton-root').click()
+        })
 
-    cy.assertMuiInput('Code', programCode)
-    cy.enterMuiInput('Code', updatedProgramCode)
-    cy.assertMuiInput('Code', updatedProgramCode)
+      cy.assertMuiInput('Code', programCode)
+      cy.enterMuiInput('Code', updatedProgramCode)
+      cy.assertMuiInput('Code', updatedProgramCode)
 
-    cy.enterMuiInput('Max Beneficiaries', updatedMaxBeneficiaries)
+      cy.enterMuiInput('Max Beneficiaries', updatedMaxBeneficiaries)
 
-    cy.enterMuiInput('Institution', updatedInstitution)
+      cy.enterMuiInput('Institution', updatedInstitution)
 
-    cy.enterMuiInput('Description', updatedDescription, 'textarea')
+      cy.enterMuiInput('Description', updatedDescription, 'textarea')
 
-    cy.get('[title="Save changes"] button').click()
+      cy.get('[title="Save changes"] button').click()
 
-    // Wait for update to complete
-    cy.get('ul.MuiList-root li div[role="progressbar"]').should('exist')
-    cy.get('ul.MuiList-root li div[role="progressbar"]').should('not.exist')
+      // Wait for update to complete
+      cy.get('ul.MuiList-root li div[role="progressbar"]').should('exist')
+      cy.get('ul.MuiList-root li div[role="progressbar"]').should('not.exist')
 
-    // Check last journal message
-    cy.get('ul.MuiList-root li').first().click()
-    cy.contains(`Update ${getProgramTerm()}`).should('exist')
-    cy.contains('Failed to update').should('not.exist')
+      // Check last journal message
+      cy.get('ul.MuiList-root li').first().click()
+      cy.contains(`Update ${getProgramTerm()}`).should('exist')
+      cy.contains('Failed to update').should('not.exist')
 
-    // Check program field values are persisted
-    cy.reload()
-    cy.checkProgramFieldValues(
-      updatedProgramCode,
-      programName,
-      updatedMaxBeneficiaries,
-      programType,
-      updatedInstitution,
-      updatedDescription,
-    )
+      // Check program field values are persisted
+      cy.reload()
+      cy.checkProgramFieldValues(
+        updatedProgramCode,
+        programName,
+        updatedMaxBeneficiaries,
+        programType,
+        updatedInstitution,
+        updatedDescription,
+      )
+    })
+  })
+})
+
+describe('Individuals and groups/households', () => {
+  before(() => {
+    // Disable maker checker
+    cy.loginAdminInterface()
+    cy.setModuleConfig('individual', 'individual-config-minimal.json')
+    cy.logoutAdminInterface()
   })
 
   it('Imports individuals and groups', function () {
-    cy.loginAdminInterface()
-    cy.setModuleConfig('individual', 'individual-config-minimal.json')
-
+    cy.login()
     cy.visit('/front/groups')
     cy.getItemCount('Group').as('initialGroupCount');
 
     cy.visit('/front/individuals')
     cy.getItemCount('Individual').as('initialIndividualCount');
 
-    cy.contains('li', 'UPLOAD').click()
-
-    cy.get('input[type="file"]').attachFile('individuals.csv');
-
-    cy.chooseMuiSelect('Workflow', 'Python Import Individuals')
-    cy.contains('button', 'Upload Individuals').click();
-
-    cy.contains('button', 'Upload Individuals').should('not.exist')
+    cy.uploadIndividualsCSV()
 
     cy.visit('/front/individuals')
     cy.getItemCount("Individual").then(newCount => {
